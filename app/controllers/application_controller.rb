47f6f5c6cfc0ee0bb100526cd8944a729d900a38
Meg Vandezande <meg.vandezande@gmail.com>
2022-05-25 16:24:55 +0200

    Set current user in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_current_user
+
+  def set_current_user
+    @current_user = session['user'] ? User.find(session['user']['id']) : User.current
+  end
 end
