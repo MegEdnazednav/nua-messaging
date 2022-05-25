3d35e5255f59aaa43a7eb34d3ed91715c53cf84f
Meg Vandezande <meg.vandezande@gmail.com>
2022-05-25 16:23:13 +0200

    add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
-  root :to => 'messages#index'
+  root :to => 'users#index'
 
-  resources :messages
-
+  resources :users, only: [:index] do
+    resource :prescription, only: [:create]
+  end
+  get 'login', to: 'sessions#set_user'
+  resource :inbox
+  resource :outbox
+  resources :messages do
+    resource :reply
+  end
 end
